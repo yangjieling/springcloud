82d7442fdc2aedb9afde14c45aea1b8405b2199c
yangjieling <yangjieling@cj.com>
2020-03-31 09:08:13 +0800

    整合zookeeper作为注册中心，新建支付微服务8004、订单微服务zk80

--- cloud-consumerzk-order80/src/main/java/com/xinxin/springcloud/OrderZkMain80.java
+++ cloud-consumerzk-order80/src/main/java/com/xinxin/springcloud/OrderZkMain80.java
@@ -3,14 +3,12 @@ package com.xinxin.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableDiscoveryClient
-public class PaymentMain8001 {
+public class OrderZkMain80 {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
+        SpringApplication.run(OrderZkMain80.class,args);
     }
 }
--- cloud-provider-payment8004/src/main/java/com/xinxin/springcloud/PaymentMain8004.java
+++ cloud-provider-payment8004/src/main/java/com/xinxin/springcloud/PaymentMain8004.java
@@ -3,14 +3,12 @@ package com.xinxin.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableDiscoveryClient
-public class PaymentMain8001 {
+public class PaymentMain8004 {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
+        SpringApplication.run(PaymentMain8004.class,args);
     }
 }
