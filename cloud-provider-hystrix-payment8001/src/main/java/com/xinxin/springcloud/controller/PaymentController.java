57e142051240765401348f1f03c878088a580e01
yangjieling <yangjieling@cj.com>
2020-04-01 16:10:41 +0800

    Hystrix服务熔断，在服务端进行服务熔断，服务降级到服务熔断到恢复链路

--- cloud-provider-hystrix-payment8001/src/main/java/com/xinxin/springcloud/controller/PaymentController.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/xinxin/springcloud/controller/PaymentController.java
@@ -28,4 +28,17 @@ public class PaymentController {
         log.info("*****result:{}",result);
         return result;
     }
+
+    /**
+     * 服务熔断
+     * http://localhost:8001/payment/circuit/32
+     * @param id
+     * @return
+     */
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
+        String result = paymentService.paymentCircuitBreaker(id);
+        log.info("***result:" + result);
+        return result;
+    }
 }
