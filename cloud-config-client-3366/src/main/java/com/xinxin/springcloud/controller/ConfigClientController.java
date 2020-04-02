33fb0379ba71895c7f4ce02cbe0bbfe3b3d2af4e
yangjieling <yangjieling@cj.com>
2020-04-02 23:02:29 +0800

    消息总线Spring cloud bus 配置刷新: 发送POST请求:(post工具也行，这里使用curl命令发送) 1、通过config server实现全部刷新： curl -X POST "http://localhost:3344/actuator/bus-refresh" 2、通过任意一个config client实现全部刷新 curl -X POST "http://localhost:3355/actuator/bus-refresh" 3、只更新指定config client curl -X POST "http://localhost:3344/actuator/bus-refresh/服务名:服务端口号"

---
+++ cloud-config-client-3366/src/main/java/com/xinxin/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.xinxin.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RefreshScope
+@RestController
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return "server.port = " + serverPort + "\t configInfo=" + configInfo;
+    }
+}
