9c387a155b0e48aaed8a2add366eede4e1e7442a
yangjieling <yangjieling@cj.com>
2020-04-03 20:13:37 +0800

    springcloud alibaba nacos服务注册中心自带负载均衡 服务提供者9001/9002，服务消费者83

---
+++ cloudalibaba-provider-payment9002/src/main/java/com/xinxin/springcloud/controller/PaymentController.java
@@ -0,0 +1,20 @@
+package com.xinxin.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id){
+        return "nacos register, serverport=" + serverPort + "\t id:" + id;
+    }
+}
